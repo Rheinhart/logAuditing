945773ba6e840dc54ed6eec647be5af5fb996281
Rothschildchen <swiftiii@gmail.com>
2015-07-23 17:36:48 +0200

    添加self.status
    refactor: global rlist->class logRefresh: self.loglist
    refactor: loadAccount: aList->userList
    refactor: ticketCheck->ticket_check
    refactor: func name->low, Class name ->upper
    refactor: url.py, index.html: ajax_refreshLog->ajax_refresh_log...

--- log_main/views.py
+++ log_main/views.py
@@ -10,11 +10,9 @@ import re
 import os
 import datetime
 
-
-
 webInfo = load_account('config.ini')     #load username and password
 p_check = z_check = s_check = ''           #pinnacle, Zhibo, Sbo object, global
-LOGLIST = []                            #global, 所有log中是waiting状态的数据解析后都储存在这里
+LOG_LIST = []                            #global, 所有log中是waiting状态的数据解析后都储存在这里
 
 
 def index(request):
@@ -38,78 +36,78 @@ def login(request,account):
     elif account == 'Sbo':
         s_check = SboCheck(webInfo['Sbo_username'],webInfo['Sbo_password'])
 
-    return HttpResponse(json.dumps(LOGLIST), content_type='application/json')
+    return HttpResponse(json.dumps(LOG_LIST), content_type='application/json')
 
 def ajax_refresh_log(request):
 
-    global LOGLIST
+    global LOG_LIST
 
-    lRefresh = LogRefresh()
-    LOGLIST=lRefresh.log_check()
-    return HttpResponse(json.dumps(LOGLIST), content_type='application/json')
+    rlog = LogRefresh()
+    LOG_LIST = rlog.log_check()
+    return HttpResponse(json.dumps(LOG_LIST), content_type='application/json')
 
 def ajax_check(request,num):
     """依照单号检查账户账单"""
 
-    global LOGLIST
+    global LOG_LIST
 
     i= int(num)
 
     #to find the account in the logList
 
-    if LOGLIST[i]['Account'] == 'Pinnacle':
-        LOGLIST[i]['Status'] = p_check.ticket_check(LOGLIST[i]['Username'],LOGLIST[i]['Ticket'])
+    if LOG_LIST[i]['Account'] == 'Pinnacle':
+        LOG_LIST[i]['Status'] = p_check.ticket_check(LOG_LIST[i]['Username'],LOG_LIST[i]['Ticket'])
 
-    elif LOGLIST[i]['Account'] == 'Zhibo':
-        LOGLIST[i]['Status'] = z_check.ticket_check(LOGLIST[i]['Username'],LOGLIST[i]['Ticket'])
+    elif LOG_LIST[i]['Account'] == 'Zhibo':
+        LOG_LIST[i]['Status'] = z_check.ticket_check(LOG_LIST[i]['Username'],LOG_LIST[i]['Ticket'])
 
-    elif LOGLIST[i]['Account'] == 'Sbo':
-        LOGLIST[i]['Status'] = s_check.ticket_check(LOGLIST[i]['Username'],LOGLIST[i]['Ticket'])
+    elif LOG_LIST[i]['Account'] == 'Sbo':
+        LOG_LIST[i]['Status'] = s_check.ticket_check(LOG_LIST[i]['Username'],LOG_LIST[i]['Ticket'])
 
-    return HttpResponse(json.dumps(LOGLIST), content_type='application/json')
+    return HttpResponse(json.dumps(LOG_LIST), content_type='application/json')
 
 
 def ajax_check_all(request, account):
     """检查所有账户账单"""
 
-    global LOGLIST
+    global LOG_LIST
 
     if account == 'All':
-        for log in LOGLIST:
+        for log in LOG_LIST:
             if log['Status'] == 'Waiting':
                 if log['Account'] == 'Pinnacle':
-                    LOGLIST['Status'] = p_check.ticket_check(log['Username'], log['Ticket'])
+                    LOG_LIST['Status'] = p_check.ticket_check(log['Username'], log['Ticket'])
                 elif log['Account'] == 'Zhibo':
-                    LOGLIST['Status'] = z_check.ticket_check(log['Username'], log['Ticket'])
+                    LOG_LIST['Status'] = z_check.ticket_check(log['Username'], log['Ticket'])
                 elif log['Account'] == 'Sbo':
-                    LOGLIST['Status'] = s_check.ticket_check(log['Username'], log['Ticket'])
+                    LOG_LIST['Status'] = s_check.ticket_check(log['Username'], log['Ticket'])
 
                 print 'Checking '+log['Account']+' '+log['Ticket']
 
     elif account == 'Pinnacle':
-        for log in LOGLIST:
+        for log in LOG_LIST:
             if log['Status'] == 'Waiting' and log['Account'] == 'Pinnacle':
                     print 'Checking '+log['Account']+' '+log['Ticket']
                     log['Status'] = p_check.ticket_check(log['Username'], log['Ticket'])
 
     elif account == 'Zhibo':
-        for log in LOGLIST:
+        for log in LOG_LIST:
             if log['Status'] == 'Waiting' and log['Account'] == 'Zhibo':
                     print 'Checking '+log['Account']+' '+log['Ticket']
                     log['Status'] = z_check.ticket_check(log['Username'], log['Ticket'])
 
     elif account == 'Sbo':
-        for log in LOGLIST:
+        for log in LOG_LIST:
             if log['Status'] == 'Waiting' and log['Account'] == 'Zhibo':
                     print 'Checking '+log['Account']+' '+log['Ticket']
-                    LOGLIST['Status'] = s_check.ticket_check(log['Username'], log['Ticket'])
+                    LOG_LIST['Status'] = s_check.ticket_check(log['Username'], log['Ticket'])
 
     #缓存当前数据
     cache=open('cache.txt','w+')
-    cPickle.dump(LOGLIST, cache)
+    cPickle.dump(LOG_LIST, cache)
     cache.close()
 
-    return HttpResponse(json.dumps(LOGLIST), content_type='application/json')
+    return HttpResponse(json.dumps(LOG_LIST), content_type='application/json')
 
 def ajax_save_log(request):
     """保存检查过的log文件"""
@@ -117,7 +115,7 @@ def ajax_save_log(request):
     WAITING_STATUS = '\xd7\xb4\xcc\xac:Waiting'            #Tag
     logname=datetime.date.today().strftime("%Y-%m-%d")
     newlogfile ='new_logs\\'+logname+'.log'                #新log地址
-    logfile = logRefresh.log_path + logname + '.log'
+    logfile = LogRefresh.log_path + logname + '.log'
     try:
         if os.path.isfile(newlogfile):
             os.remove(newlogfile)
@@ -127,7 +125,7 @@ def ajax_save_log(request):
             line = line.split()
             if WAITING_STATUS in line:
                 ticket = re.findall("\xb5\xa5\xba\xc5:(.*)", line[5])[0] #paser ticket
-                for logline in LOGLIST:
+                for logline in LOG_LIST:
                     if ticket == logline['Ticket']:
                         NEWSTATUS = (u'状态:').encode('gb2312')+logline['Status'].encode('gb2312')
                         line[6]=line[6].replace(WAITING_STATUS, NEWSTATUS)
@@ -141,5 +139,5 @@ def ajax_save_log(request):
     finally:
         log.close()
 
-    return HttpResponse(json.dumps(LOGLIST), content_type='application/json')
+    return HttpResponse(json.dumps(LOG_LIST), content_type='application/json')
 
